a66f260ae9718dcf36552588ff875bf370602756
Jack Halten Fahnestock <buttface@jackf.me>
2018-08-23 14:04:00 -0400

    small under-the-hood fixes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom';
 import App from './App'
 import './components/config/global'
-import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(
   <App />,
